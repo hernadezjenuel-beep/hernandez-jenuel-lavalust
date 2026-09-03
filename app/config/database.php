9a1d8f89ea33d1539d02228ada8708c1c99e766f
hernadezjenuel-beep <hernadezjenuel@gmail.com>
2026-09-03 14:19:26 +0800

    Modify database configuration to empty values
    Updated database configuration to use empty values.

--- app/config/database.php
+++ app/config/database.php
@@ -58,13 +58,13 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 */
 
     $database['main'] = [
-    'driver'   => 'mysql',
-    'hostname' => 'localhost',
-    'port'     => '3306',
-    'username' => 'root',
+    'driver'   => '',
+    'hostname' => '',
+    'port'     => '',
+    'username' => '',
     'password' => '',
-    'database' => 'mydb',
-    'charset'  => 'utf8mb4',
+    'database' => '',
+    'charset'  => '',
     'dbprefix' => '',
 ];
 ?>
